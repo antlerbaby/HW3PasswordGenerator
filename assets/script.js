d057b87bdb3a26e84ac1cd2348a46e77ba95e017
Jeannette Kunzen <57763536+antlerbaby@users.noreply.github.com>
2020-01-05 21:34:45 -0500

    pseudo-code outline added to js file

---
+++ assets/script.js
@@ -0,0 +1,45 @@
+/* 1.) Define and Randomize choice from Character Strings= 
+        Special Characters
+        Numeric Characters
+        Lowercase Characters
+        Uppercase Characters */
+
+function
+
+// 2.) Combine the functions somehow to loop through the specified number when called
+
+function
+
+/* 3.) Define and connect DOM elements from HTML 
+                                            id="pwLength"
+                                            id="specialCharacters" 
+                                            id="numCharacters"
+                                            id="lowCharacters"
+                                            id="upCharacters"
+                                            id="runProgramButt"
+                                            id="revealPassword"  */
+
+const
+
+/* 4.) Click Events- include behavior for checkbox input types to trigger functions
+                                            id="specialCharacters" 
+                                            id="numCharacters"
+                                            id="lowCharacters"
+                                            id="upCharacters"  */
+
+.addEventListener //click checkboxes?
+
+/* 5.) Click Event- click button id="runProgramButt" 
+       which collects and displays the resulting string into id="revealPassword" */
+
+.addEventListener //click button
+        // length (required between 8 and 128)
+        // specialCharacters checked true? 
+        // numCharacters checked true?
+        // lowCharacters checked true?
+        // upCharacters checked true?
+        // remove false
+
+revealPassword.innerText = /* Add above together and generate final password string into "Password will appear here..." */
+
+/* 6.) Remove copy/paste function from HTML and collect here (?) */
